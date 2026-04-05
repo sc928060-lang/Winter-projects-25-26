3f3adfc223c8282f609f48be3071dba6f685a5cf
coder-devdas <prathamr24@iitk.ac.in>
2026-04-06 00:05:13 +0530

    final project submition complete

---
+++ Deep Neural Network Compression & CV/Project/compression/conv2d.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from sklearn.cluster import KMeans
+class modified_linear(nn.Conv2d):
+    def __init__(self, in_features, out_features, bias = True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+    def prune(self,threshold):
+        #this function should prune the weights which are below threshold
+        pass
+    def quantize(self,k):
+        '''cluster the weights in k different cluster, and then use 2 things, 
+        one is a cluster map which will be o(nxn) uint8 type and one array of clusters which wil
+        be a O(k) sized fload 34 
+
+        note that SK-learn is a cpu library!
+        '''
+        pass
+    def forward(self, input):
+        if(self.mode == 'normal'):
+            print("hello")
+        elif(self.mode == 'prune'):
+            print("hello")
+        elif(self.mode == 'quantize'):
+            print("hello")
+        return
+
+    
---
+++ Deep Neural Network Compression & CV/Project/main.py
@@ -0,0 +1,54 @@
+import torch
+from models.mnist import mnist_model
+from data.data_loader import MNIST_loader
+from utils.training import train_model,evaluate,train_and_eval
+from compression.pruning import prune_model,quantize_model
+from utils.loading import load_csr_from_npz,save_model_npz,load_model_from_npz
+from config import config_device
+
+path = 'data' #path to the data folder
+device = config_device()
+
+train_loader,test_loader = MNIST_loader(path)
+model = mnist_model()
+model = model.to(device)
+
+train_and_eval(model,train_loader,test_loader,device,epochs=300)
+prune_model(model,0.90)
+train_and_eval(model,train_loader,test_loader,device,epochs=50)
+
+def count_mask_sparsity(model):
+    total = 0
+    zeros = 0
+    for module in model.modules():
+        if hasattr(module, "mask"):
+            total += module.mask.numel()
+            zeros += (module.mask == 0).sum().item()
+    print(f"Masked sparsity: {100 * zeros / total:.2f}%")
+
+count_mask_sparsity(model)
+
+torch.save(model.state_dict(), "compressed_models/model.pth")
+
+quantize_model(model,16) #because the minium size of pytorch is uint8 you can have till 256 weights
+train_and_eval(model,train_loader,test_loader,device,epochs=1)
+
+npz_path = "compressed_models/compressed.npz"
+save_model_npz(model,npz_path)
+
+model2 = mnist_model()
+model2 = load_model_from_npz(model,npz_path,device)
+train_loader,test_loader = MNIST_loader(path)
+model2 = model2.to(device)
+print(evaluate(model2,test_loader,device))
+print(model.classifier[0].__dict__.keys())
+print(model.classifier[0].weight)
+
+
+'''
+2 things
+1) runtime memory report 
+2) disk space 
+
+# dont use you own laptop while doing this 
+'''
